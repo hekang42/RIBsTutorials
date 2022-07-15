449437bf17a746caf23bb714715a53e8a7aaadb7
hekang <hekang@student.42seoul.kr>
2022-07-15 12:56:14 +0900

    [feat] Passing the player names to the OffGame scope using the DI tree

---
+++ tutorial3/TicTacToe/LoggedIn/ScoreStream.swift
@@ -0,0 +1,55 @@
+//
+//  ScoreStream.swift
+//  TicTacToe
+//
+//  Created by A11548 on 2022/07/15.
+//  Copyright © 2022 Uber. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+struct Score {
+    let player1Score: Int
+    let player2Score: Int
+
+    static func equals(lhs: Score, rhs: Score) -> Bool {
+        return lhs.player1Score == rhs.player1Score && lhs.player2Score == rhs.player2Score
+    }
+}
+
+protocol ScoreStream: class {
+    var score: Observable<Score> { get }
+}
+
+protocol MutableScoreStream: ScoreStream {
+    func updateScore(withWinner winner: PlayerType)
+}
+
+class ScoreStreamImpl: MutableScoreStream {
+
+    var score: Observable<Score> {
+        return variable
+            .asObservable()
+            .distinctUntilChanged { (lhs: Score, rhs: Score) -> Bool in
+                Score.equals(lhs: lhs, rhs: rhs)
+            }
+    }
+
+    func updateScore(withWinner winner: PlayerType) {
+        let newScore: Score = {
+            let currentScore = variable.value
+            switch winner {
+            case .player1:
+                return Score(player1Score: currentScore.player1Score + 1, player2Score: currentScore.player2Score)
+            case .player2:
+                return Score(player1Score: currentScore.player1Score, player2Score: currentScore.player2Score + 1)
+            }
+        }()
+        variable.accept(newScore)
+    }
+
+    // MARK: - Private
+
+    private let variable = BehaviorRelay<Score>(value: Score(player1Score: 0, player2Score: 0))
+}
--- tutorial3/TicTacToe/OffGame/OffGameViewController.swift
+++ tutorial3/TicTacToe/OffGame/OffGameViewController.swift
@@ -27,8 +27,12 @@ protocol OffGamePresentableListener: AnyObject {
 final class OffGameViewController: UIViewController, OffGamePresentable, OffGameViewControllable {
 
     weak var listener: OffGamePresentableListener?
+    private let player1Name: String
+    private let player2Name: String
 
-    init() {
+    init(player1Name: String, player2Name: String) {
+        self.player1Name = player1Name
+        self.player2Name = player2Name
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -41,8 +45,48 @@ final class OffGameViewController: UIViewController, OffGamePresentable, OffGame
 
         view.backgroundColor = UIColor.yellow
         buildStartButton()
+        buildPlayerLabels()
     }
 
+    private func buildPlayerLabels() {
+        let labelBuilder: (UIColor, String) -> UILabel = { (color: UIColor, text: String) in
+            let label = UILabel()
+            label.font = UIFont.boldSystemFont(ofSize: 35)
+            label.backgroundColor = UIColor.clear
+            label.textColor = color
+            label.textAlignment = .center
+            label.text = text
+            return label
+        }
+
+        let player1Label = labelBuilder(UIColor.blue, player1Name)
+        view.addSubview(player1Label)
+        player1Label.snp.makeConstraints { (maker: ConstraintMaker) in
+            maker.top.equalTo(self.view).offset(70)
+            maker.leading.trailing.equalTo(self.view).inset(20)
+            maker.height.equalTo(40)
+        }
+
+        let vsLabel = UILabel()x
+        vsLabel.font = UIFont.systemFont(ofSize: 25)
+        vsLabel.backgroundColor = UIColor.clear
+        vsLabel.textColor = UIColor.darkGray
+        vsLabel.textAlignment = .center
+        vsLabel.text = "vs"
+        view.addSubview(vsLabel)
+        vsLabel.snp.makeConstraints { (maker: ConstraintMaker) in
+            maker.top.equalTo(player1Label.snp.bottom).offset(10)
+            maker.leading.trailing.equalTo(player1Label)
+            maker.height.equalTo(20)
+        }
+
+        let player2Label = labelBuilder(UIColor.red, player2Name)
+        view.addSubview(player2Label)
+        player2Label.snp.makeConstraints { (maker: ConstraintMaker) in
+            maker.top.equalTo(vsLabel.snp.bottom).offset(10)
+            maker.height.leading.trailing.equalTo(player1Label)
+        }
+    }
     // MARK: - Private
 
     private func buildStartButton() {
